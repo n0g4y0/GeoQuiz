41b60f4b103712f875b79d5ad01dbc70f310c13c
n0g4y0 <n0g4y0@gmail.com>
2019-08-16 16:58:59 -0400

    agregando ID's a los botones, y añadiendolos en el MAinActivity, tambien se agrego sus eventos CLICKS

--- app/src/main/java/io/github/n0g4y0/geoquiz/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/geoquiz/MainActivity.kt
@@ -2,11 +2,30 @@ package io.github.n0g4y0.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            // Do something in response to the click here
+        }
+
+
+
     }
 }
