eb10549a23194c3225b55b532a5f218258000ae9
n0g4y0 <n0g4y0@gmail.com>
2019-09-05 12:56:00 -0400

    ejemplos, llamadas a los CALLBACKS, y cuando son llamadas, son mostradas por el LOG, en cada uno

--- app/src/main/java/io/github/n0g4y0/geoquiz/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/geoquiz/MainActivity.kt
@@ -62,6 +62,33 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
+
+
     private fun updateQuestion(){
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
