4369ed8ca2934ded8550314f29c5389702de13d7
n0g4y0 <n0g4y0@gmail.com>
2019-09-08 17:02:56 -0400

    intentando mover, poco a poco , la logica de la APP, a la clase VIEWMODEL, para que el ACTIVITY se ocupe solo de las vistas

--- app/src/main/java/io/github/n0g4y0/geoquiz/QuizViewModel.kt
+++ app/src/main/java/io/github/n0g4y0/geoquiz/QuizViewModel.kt
@@ -7,12 +7,39 @@ private const val TAG = "QuizViewModel"
 
 class QuizViewModel : ViewModel() {
 
-    init {
-        Log.d(TAG,"ViewModel instance created. ")
+    /*
+    * codigo traido desde el MAinActivity, para que pueda guardarse, al momento de cambiar
+    * la configuracion de la APP (ROTACION), ya que el VIEWMODEL sobrevive a la destruccion de la APP,
+    * cuando esta subre un cambio de configuracion.
+    * */
+
+    // quitandole la palabra reservada "Private", para poder accesar a su actual posicion.
+    var currentIndex = 0
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia,true),
+        Question(R.string.question_oceans,true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa,false),
+        Question(R.string.question_americas,true),
+        Question(R.string.question_asia,true))
+
+    /*
+    * agregando funciones, para retornar valores al MainActivity:
+    *
+    * */
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext(){
+        currentIndex = (currentIndex + 1) % questionBank.size
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.d(TAG,"ViewModel instance about to be destroyed")
-    }
+
+
+
 }
