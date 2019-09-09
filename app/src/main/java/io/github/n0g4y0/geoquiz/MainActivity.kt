bc23da9487bc4aba385144219160f163786d7a0a
n0g4y0 <n0g4y0@gmail.com>
2019-09-08 22:45:56 -0400

    se asigna el valor del INDEXACTUAL, desde el BUNDLE, funciona correctamente, al LIBERAR MEMORIA, en el modo desarrollador del device.

--- app/src/main/java/io/github/n0g4y0/geoquiz/MainActivity.kt
+++ app/src/main/java/io/github/n0g4y0/geoquiz/MainActivity.kt
@@ -35,6 +35,9 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG,"onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
+
 
 
         trueButton = findViewById(R.id.true_button)
