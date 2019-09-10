78a40f2df9baff3290d73516b7d90b7d6c22dfe4
n0g4y0 <n0g4y0@gmail.com>
2019-09-10 12:07:06 -0400

    configurando una funcion, para poder manipular los EXTRAS, que un ACTIVITY necesita

--- app/src/main/java/io/github/n0g4y0/geoquiz/CheatActivity.kt
+++ app/src/main/java/io/github/n0g4y0/geoquiz/CheatActivity.kt
@@ -1,5 +1,7 @@
 package io.github.n0g4y0.geoquiz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,5 +11,13 @@ class CheatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext,CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
     }
 }
